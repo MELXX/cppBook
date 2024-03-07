e592aec92cabc81d0f24e9540ee42601c5206ff3
kmgwenya <kmgwenya@retrorabbit.co.za>
2024-03-07 13:43:55 +0200

    code

--- module8.cpp
+++ module8.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 
 using namespace std;
+class Vehicle
+{
+    public:
+    int passengers;
+    int fuelCap;
+    int kmpl;
 
+};
 int main()
 {
-
+    Vehicle v; //creates instance??
+    v.fuelCap = 100;
+    int i = v.fuelCap;
+    cout << i;
 }
+// class-name
+// {
+// private data and functions
+// public:
+// public data and functions
+// } object-list;
